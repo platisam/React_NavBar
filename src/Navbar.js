0d3d10f181f6a9ae24e58b9eb2ed2ef7e3896dfd
platisam <platisam@gmail.com>
2023-09-14 17:11:34 +0200

    dynamically adding a toggle navbar classes

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaBars, FaTwitter } from "react-icons/fa";
 import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
+
+        <div
+          className={` ${
+            showLinks ? "links-container show-container" : "links-container"
+          }`}
+        >
           <ul className="links">
             {links.map((link) => {
               const { id, url, text } = link;
@@ -25,6 +35,7 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+
         <ul className="social-icons">
           {social.map((socialIcon) => {
             const { id, url, icon } = socialIcon;
