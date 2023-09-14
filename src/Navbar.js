792c48c60ec8f25497a47c03f263c9c3c020fd2f
platisam <platisam@gmail.com>
2023-09-14 10:43:11 +0200

    setting links and social icons to Navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,6 @@
+import React from "react";
+import { FaBars, FaTwitter } from "react-icons/fa";
+import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
@@ -6,10 +9,43 @@ const Navbar = () => {
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle"></button>
+          <button className="nav-toggle">
+            <FaBars />
+          </button>
         </div>
-        <div className="links-container show-container"></div>
-        <ul className="social-icons"></ul>
+        <div className="links-container show-container">
+          <ul className="links">
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+            <li>
+              <a href="#">contact</a>
+            </li>
+            <li>
+              <a href="#">products</a>
+            </li>
+          </ul>
+        </div>
+        <ul className="social-icons">
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.twitter.com">
+              <FaTwitter />
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
